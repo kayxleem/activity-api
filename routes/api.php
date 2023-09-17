92ee53c81b254408ae6e124d3c04d37b86a1b596
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-17 21:34:19 +0100

    added routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\API\UserActivityController;
+use App\Http\Controllers\API\Usercontroller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('user')->middleware('auth:sanctum')->group(function () {
+    //Route::get('/activities',[Usercontroller::class,'userActivity']);
+    Route::get('/activities',UserActivityController::class,);
+
+});
+Route::post('user/register', [Usercontroller::class,'register'])->name('user.register');
+
+Route::post('user/login', [Usercontroller::class,'login'])->name('user.login');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminActivityController;
+use App\Http\Controllers\Admin\Admincontroller;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Route::get('/admin', [Admincontroller::class, 'index'])->name('admin.dashboard');
+// Route::get('/admin/login', [Admincontroller::class, 'login'])->name('admin.show.login');
+// Route::post('/admin/login', [Admincontroller::class, 'processLogin'])->name('admin.login');
+
+Route::prefix('/admin')->group(function ($route) {
+    //$route->get('/', Admincontroller::class);
+    $route->get('/',  [Admincontroller::class, 'index'])->name('admin');
+    $route->get('/login', [Admincontroller::class, 'index'])->name('admin.show.login');
+    $route->post('/login', [Admincontroller::class, 'login'])->name('admin.login');
+    $route->get('/logout', [Admincontroller::class, 'logout'])->name('admin.logout');
+
+
+    Route::middleware('admin')->group(function(){
+        //Route::get('/dashboard',  [Admincontroller::class, 'dashboard'])->name('admin.dashboard');
+        Route::get('/dashboard',  [AdminActivityController::class, 'getAllActivities'])->name('admin.dashboard');
+        Route::get('/createActivity',  [AdminActivityController::class, 'createActivityView'])->name('admin.create.activity.view');
+        //Route::get('/activities',  [AdminActivityController::class, 'getAllActivities'])->name('admin.activities')->middleware('admin');
+    });
+
+});
+
+require __DIR__.'/auth.php';
